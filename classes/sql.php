b7c2cacb5a1f4842498e6b7d4f20deb955565d42
Henrik <spearman_cs@hotmail.com>
2011-04-28 19:37:57 +0200

    Endret litt paa sql-klassen

--- classes/sql.php
+++ classes/sql.php
@@ -2,12 +2,12 @@
 
 class sql extends mysqli
 {
-	function __construct($dbinfo)
+	function __construct()
 	{
-		parent::__construct($dbinfo[0], $dbinfo[1], $dbinfo[2], $dbinfo[3], $dbinfo[4]);
+		parent::__construct("84.209.111.93","webprosjekt","web123","web_prosjekt","3306");
 
-		//if(mysqli_connect_error())
-		//	die("Kunne ikke opprette tilkobling til databasen: (".mysqli_connect_errno().") ".mysqli_connect_error());
+		if(mysqli_connect_error())
+			die("Kunne ikke opprette tilkobling til databasen: (".mysqli_connect_errno().") ".mysqli_connect_error());
 	}
 	
 	function close()
--- index.php
+++ index.php
@@ -6,12 +6,10 @@ $gjennomIndex=true;
 $side=str_replace("/","",$_REQUEST['side']);
 $denied_includes=array("","blabla"); // Sider som ikke skal kunne vises som andre sider
 
-$dbinfo=array("84.209.111.93","webprosjekt","web123","web_prosjekt","3306");
-
 require_once"_functions.php";
 require_once"_classes.php";
 
-$db=new sql($dbinfo);
+$db=new sql();
 if($db->connect_error)
 	echo"nei";
 else
