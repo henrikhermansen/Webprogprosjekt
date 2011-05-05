b1b35256b99b28ee1f5e90c97f7867224793f497
Hiran Piapo <hirpia@gmail.com>
2011-05-05 10:03:31 +0200

    Ordnet litt paa admin-sidene

---
+++ include/admin_legg_til_ny_vare.php
@@ -0,0 +1,16 @@
+<h2>Legg til ny vare</h2>
+
+<form name="ny_vare" action="" method="post">
+    
+        <p><label>Vare navn</label><input type="text" name="vnavn"/></p>
+        <p><label>Pris</label><input type="text" name="vpris"/></p>
+        <p><label>Antall varer</label><input type="text" name="vantall"/></p>
+        <p>Beskrivelse<br/><textarea cols="40" rows="10" name="beskrivelse"></textarea></p>
+
+        <p><input type="submit" name="legg_til" value="Legg til vare"</p>
+
+</form>
+
+<?php
+
+?>
--- include/admin_varer.php
+++ include/admin_varer.php
@@ -1,3 +1,14 @@
 <?php if(!$gjennomIndex) die("Access denied.");?>
 
 <h2>Varer</h2>
+
+    <a href="index.php?side=admin_legg_til_ny_vare">Legg til ny vare</a>
+    
+
+  
+
+
+
+
+    
+
