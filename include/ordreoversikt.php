e7b30d3abf6f7acc84e2a78e5094b5f9d7659a6c
Henrik <spearman_cs@hotmail.com>
2011-05-06 21:10:42 +0200

    Fikset en bagatell i ordreoversikten

--- include/ordreoversikt.php
+++ include/ordreoversikt.php
@@ -24,10 +24,10 @@ foreach ($ordrelinjer as $ordrelinje)
 {
     echo "<tr><td>".$ordrelinje[0]."</td><td>".$ordrelinje[1]."</td><td>".number_format($ordrelinje[2],2,',','.')."</td><td>".$ordrelinje[3]."</td><td>25&#37;</td><td>".number_format($ordrelinje[4],2,',','.')."</td></tr>";
 }
-echo "<tr><td></td><td></td><td></td><td></td><td><strong>Sum varer:</strong></td><td>".number_format($ordre->getOrdretotal(),2,',','.')."</td></tr>";
-echo "<tr><td></td><td></td><td></td><td></td><td><strong>Frakt:</strong></td><td>120,00</td></tr>";
-echo "<tr><td></td><td></td><td></td><td></td><td><strong>Herav MVA (25&#37;):</strong></td><td>".number_format((($ordre->getOrdretotal()+120)*0.2),2,',','.')."</td></tr>";
-echo "<tr><td></td><td></td><td></td><td></td><td><strong>TOTALT:</strong></td><td>".number_format(($ordre->getOrdretotal()+120),2,',','.')."</td></tr>";
+echo "<tr><td colspan=\"4\"></td><td><strong>Sum varer:</strong></td><td>".number_format($ordre->getOrdretotal(),2,',','.')."</td></tr>";
+echo "<tr><td colspan=\"4\"></td><td><strong>Frakt:</strong></td><td>120,00</td></tr>";
+echo "<tr><td colspan=\"4\"></td><td><strong>Herav MVA (25&#37;):</strong></td><td>".number_format((($ordre->getOrdretotal()+120)*0.2),2,',','.')."</td></tr>";
+echo "<tr><td colspan=\"4\"></td><td><strong>TOTALT:</strong></td><td>".number_format(($ordre->getOrdretotal()+120),2,',','.')."</td></tr>";
 ?>
 </table>
 <p><a href="index.php?side=minkonto&amp;kontoside=ordre">Tilbake til mine ordre</a></p>
