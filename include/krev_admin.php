cbfba20485f3780bf10bc5dee6b8a4110f829a7e
Henrik <spearman_cs@hotmail.com>
2011-05-06 15:01:24 +0200

    Ordnet litt sikkerhet i forhold til aa kreve innlogging av kunde/administrator

---
+++ include/krev_admin.php
@@ -0,0 +1,5 @@
+<?php if(!$gjennomIndex) die("Access denied.");?>
+
+<p class="feilmelding">Denne siden krever innlogging som administrator. Vennligst logg deg inn.</p>
+
+<?php include"admlogginn.php"; ?>
---
+++ include/krev_innlogging.php
@@ -0,0 +1,5 @@
+<?php if(!$gjennomIndex) die("Access denied.");?>
+
+<p class="feilmelding">Denne siden krever innlogging. Vennligst logg inn eller <a href="index.php?side=nykunde">registrer deg som ny kunde<a>.</p>
+
+<?php include"logginn.php"; ?>
